4a73ba0f0fadb65da43b6231ebcfb5b37566189f
Dawid Iwaniuk <dawuso@gmail.com>
2021-03-14 16:17:55 +0100

    Dodaj pliki projektów.

---
+++ AddressBook_PS2/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AddressBook_PS2.Models
+{
+    public class Address
+    {
+        [Display(Name = "Ulica")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage ="Polejest obowiązkowe")]
+        public string Street { get; set; }
+        [Display(Name = "Kod pocztowy")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Miasto")]
+        public string City { get; set; }
+        [Display(Name = "Numer")]
+        public int Number { get; set; }
+    }
+}
